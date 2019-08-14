fb3004e3a992f44d738a10161fb4a5af55210ae1
Santiago Herrera <santiagoherrera@seven4n.com>
2019-08-13 21:54:01 -0500

    Se agrega ruta de inputs

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/route/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English, 
+        const Locale('es'), // English, 
+      ],
       // home: HomePage(),
       initialRoute: '/',
       routes: getApplicationRoutes(),
